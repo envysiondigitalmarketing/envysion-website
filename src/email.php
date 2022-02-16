50196e8bd2ab46894a6a4963c924a2631a754cc2
Chriscastanuela <chriscastanuela@ymail.com>
2022-02-15 23:11:06 -0700

    complete php

--- src/email.php
+++ src/email.php
@@ -1,5 +1,26 @@
 <?php
-    $firstName = $_POST['first-name'];
-    $lastName = $_POST['last-name'];
-    $company = $_POST['company'];
+    if(isset($_POST['submit'])) {
+        $firstName = $_POST['first-name'];
+        $lastName = $_POST['last-name'];
+        $company = $_POST['company'];
+        $interestedIn = $_POST['interested-in'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+    
+        $email_from = 'contact@envysiondm.com';
+        $email_subject = "Interested in $interestedIn";
+        $email_body = "Name: $firstName $lastName.\n".
+                        "Company: $company.\n".
+                            "Contacts: $email $phone.\n".
+                                "Interested in: $interestedIn.\n";
+        $to = "contact@envysiondm.com";
+        $headers = "From: $email \r\n";
+        $headers .= "Reply-To: $email \r\n";
+
+        mail($to, $email_subject, $email_body, $headers);
+
+        if($error == false) {
+            echo '<script>alert("Thank you for your inquiry, we will be in touch.")</script>';
+        }
+    }
 >
