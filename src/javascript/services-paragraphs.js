1d78aff7157c608fe3b11d9348e88ce58b4cbe48
Chriscastanuela <chriscastanuela@ymail.com>
2022-12-11 22:52:31 -0700

    relocate services paragraphs - websites

---
+++ src/javascript/services-paragraphs.js
@@ -0,0 +1,8 @@
+const paragraphs = {
+  samples: {
+    websites: `Your website is your digital hub, the place everyone goes when they want to learn more about your business. In many ways [...] <a id='reference-link' href='./services.html''>Read More &#8640;</a>`,
+  },
+  full: {
+    websites: `Your website is your digital hub, the place everyone goes when they want to learn more about your business. In many ways your website will be a chance at a first impression. Let’s make it a good one. Every site we create is designed specifically to encourage customers to take the next step and reach out to your business.`,
+  },
+};
