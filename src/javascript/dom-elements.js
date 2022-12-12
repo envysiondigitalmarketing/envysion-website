9d1de0e040cb3e19263df64e73e083c136965189
Chriscastanuela <chriscastanuela@ymail.com>
2022-12-11 22:48:22 -0700

    add dom elements file

---
+++ src/javascript/dom-elements.js
@@ -0,0 +1,84 @@
+const xButton = document.getElementById('nav__button-x');
+
+const menuCover = document.getElementById('menu-cover');
+
+const hero = document.getElementById('hero-button');
+const about = document.getElementById('about-button');
+const services = document.getElementById('services-button');
+const whyEnvysion = document.getElementById('why-envysion-button');
+const pricing = document.getElementById('pricing-button');
+const contact = document.getElementById('contact-button');
+
+const websitesHeader = document.querySelector('.services__header--websites');
+const seoHeader = document.querySelector('.services__header--seo');
+const emailAndSMSHeader = document.querySelector(
+  '.services__header--email-and-sms'
+);
+const advertsHeader = document.querySelector('.services__header--adverts');
+const allHeaders = document.querySelector('.services__header');
+
+const twoWebsitesHeader = document.querySelector(
+  '.services-II__header--websites'
+);
+const twoSeoHeader = document.querySelector('.services-II__header--seo');
+const twoEmailAndSMSHeader = document.querySelector(
+  '.services-II__header--email-and-sms'
+);
+const twoAdvertsHeader = document.querySelector(
+  '.services-II__header--adverts'
+);
+const twoAllHeaders = document.querySelector('.services-II__header');
+
+const websitesParagraph = document.querySelector(
+  '.services__paragraph--websites'
+);
+const seoParagraph = document.querySelector('.services__paragraph--seo');
+const emailAndSMSParagraph = document.querySelector(
+  '.services__paragraph--email-and-sms'
+);
+const advertsParagraph = document.querySelector(
+  '.services__paragraph--adverts'
+);
+
+const twoWebsitesParagraph = document.querySelector(
+  '.services-II__paragraph--websites'
+);
+const twoSeoParagraph = document.querySelector('.services-II__paragraph--seo');
+const twoEmailAndSMSParagraph = document.querySelector(
+  '.services-II__paragraph--email-and-sms'
+);
+const twoAdvertsParagraph = document.querySelector(
+  '.services-II__paragraph--adverts'
+);
+
+const quoraCreativeLink = document.querySelector('.quoracreative-link');
+
+export default domElements = [
+  xButton,
+  menuCover,
+  hero,
+  about,
+  services,
+  whyEnvysion,
+  pricing,
+  contact,
+  websitesHeader,
+  seoHeader,
+  emailAndSMSHeader,
+  advertsHeader,
+  allHeaders,
+  twoWebsitesHeader,
+  twoSeoHeader,
+  twoEmailAndSMSHeader,
+  twoAdvertsHeader,
+  twoAllHeaders,
+  websitesParagraph,
+  seoParagraph,
+  emailAndSMSParagraph,
+  advertsParagraph,
+  twoWebsitesParagraph,
+  twoSeoParagraph,
+  twoEmailAndSMSParagraph,
+  twoAdvertsParagraph,
+  quoraCreativeLink,
+];
