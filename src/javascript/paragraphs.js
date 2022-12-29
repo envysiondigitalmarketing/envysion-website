8d08f16b89646420dcb301f0ad434b12ce19d2af
Chriscastanuela <chriscastanuela@ymail.com>
2022-12-29 13:18:27 -0700

    add page - marketing strategy

--- src/javascript/paragraphs.js
+++ src/javascript/paragraphs.js
@@ -1,10 +1,40 @@
 const paragraphs = {
-  full: {
-    adverts: `Harness the power of Google, Facebook, or Instagram ads to drive traffic to your website. These platforms have been refined over time to become great options for gaining visibility and reaching out to potential customers within your niche. Don't have content for your ads? Envysion has you covered. Just let us know what product or service you would like advertised and we will do the rest.`,
-    emailAndSms: `Email marketing and SMS marketing are great tools to generate new leads and keep your current customers up to date with the latest information about your business. A recent study has shown that for every dollar your company spends on email marketing alone you can expect a return of $42 <a id='reference-link' onclick='window.open("https://dma.org.uk/uploads/misc/marketers-email-tracker-2019.pdf", "_blank", "width=800, height=800");'>(DMA)</a> and 98% of text messages sent are viewed by the receiver <a id='reference-link' onclick='window.open("https://www.gartner.com/en/marketing/insights/articles/tap-into-the-marketing-power-of-sms", "_blank", "width=800, height=800");'>(Gartner Marketing and Communications)</a>. If you’re interested in reaching out to new potential clients or you simply want a direct communication channel with your customers that you could use to launch new sales or products you can not go wrong with email marketing or SMS marketing. Not sure which one is right for you? No problem, give us a call and we can find a plan that best suits your needs.`,
-    seo: `Did you know that 68% of online experiences begin with a search engine (<a id='reference-link' onclick='window.open("https://videos.brightedge.com/research-report/BrightEdge_ChannelReport2019_FINAL.pdf", "_blank", "width=800, height=800");'>BrightEdge</a>), 92.96% of global traffic comes from Google (<a id='reference-link' onclick='window.open("https://sparktoro.com/blog/2018-search-market-share-myths-vs-realities-of-google-bing-amazon-facebook-duckduckgo-more/", "_blank", "width=800, height=800");'>Sparktoro</a>), and only 0.78% of Google searchers click on results from the 2nd page (<a id='reference-link' onclick='window.open("https://backlinko.com/google-ctr-stats", "_blank", "width=800, height=800");'>Backlinko</a>)? If you want customers who are searching for your product or service to find you it is imperative that you use search engine optimization to bring your company up in the search results and avoid becoming a needle in a haystack.`,
-    websites: `Your website is your digital hub, the place everyone goes when they want to learn more about your business. In many ways your website will be a chance at a first impression. Let’s make it a good one. Every site we create is designed specifically to encourage customers to take the next step and reach out to your business.`,
-  },
+  adverts: `Harness the power of Facebook and Instagram ads to drive traffic to your website. These platforms have been refined over time to become great options for gaining visibility and reaching out to potential customers within your niche. Don't have content for your ads? Envysion has you covered. Just let us know what product or service you would like advertised and we will do the rest.`,
+  blogging: `Our team of skilled writers will create high-quality, engaging content for your blog that resonates with your target audience and showcases your expertise. We'll work with you to understand your goals and needs, and develop a customized content plan to help you achieve them.
+
+  With our Blogging service, you can expect:
+  
+  Engaging, well-written blog posts that reflect your brand voice and style
+  Targeted, SEO-optimized content to help increase your search rankings and online visibility
+  Regular updates to keep your blog fresh and interesting for your readers
+  Expert guidance and support to help you maximize the impact of your blog
+  Don't miss out on the many benefits of blogging. Contact us today to learn more about how our Blogging service can help you succeed online.`,
+  googleMyBusiness: `
+  Are you looking to improve your local search visibility and reach more customers in your area? Do you want to stand out from the competition and make it easy for potential customers to find and contact your business? Our Google "My Business" Optimization service can help.
+
+  Let us optimize your Google "My Business" listing to ensure that it accurately reflects your business and its offerings. We'll also work with you to improve your online presence and reputation through techniques such as ratings management.
+
+  With our Google "My Business" Optimization service, you can expect:
+
+  A fully optimized Google "My Business" listing that accurately reflects your business and its offerings
+  Increased local search visibility, making it easier for potential customers to find and contact your business
+  Improved online reputation and customer trust through techniques such as ratings management
+  Expert guidance and support to help you make the most of your Google "My Business" listing
+  Don't let your business get lost in the sea of local search results. Contact us today to learn more about how our Google "My Business" Optimization service can help you succeed.
+  `,
+  marketingStrategy: ``,
+  socialMediaBranding: `Our social media experts will work with you to create a cohesive and compelling brand identity across all your social media channels. We'll help you develop a strong, consistent brand voice and style, and create engaging content that resonates with your target audience.
+
+  Our Social Media Branding service includes:
+  
+  A comprehensive social media strategy that aligns with your overall branding and marketing goals
+  Expert guidance and support to help you effectively manage and optimize your social media presence
+
+  Don't let your brand get lost in the noise of social media. Contact us today to learn more about how our Social Media Branding service can help you stand out and succeed.
+  `,
+  websites: `Your website is your digital hub, the place everyone goes when they want to learn more about your business. In many ways your website will be a chance at a first impression. Let’s make it a good one. Every site we create is designed specifically to encourage customers to take the next step and reach out to your business.`,
+  // emailAndSms: `Email marketing and SMS marketing are great tools to generate new leads and keep your current customers up to date with the latest information about your business. A recent study has shown that for every dollar your company spends on email marketing alone you can expect a return of $42 <a id='reference-link' onclick='window.open("https://dma.org.uk/uploads/misc/marketers-email-tracker-2019.pdf", "_blank", "width=800, height=800");'>(DMA)</a> and 98% of text messages sent are viewed by the receiver <a id='reference-link' onclick='window.open("https://www.gartner.com/en/marketing/insights/articles/tap-into-the-marketing-power-of-sms", "_blank", "width=800, height=800");'>(Gartner Marketing and Communications)</a>. If you’re interested in reaching out to new potential clients or you simply want a direct communication channel with your customers that you could use to launch new sales or products you can not go wrong with email marketing or SMS marketing. Not sure which one is right for you? No problem, give us a call and we can find a plan that best suits your needs.`,
+  // seo: `Did you know that 68% of online experiences begin with a search engine (<a id='reference-link' onclick='window.open("https://videos.brightedge.com/research-report/BrightEdge_ChannelReport2019_FINAL.pdf", "_blank", "width=800, height=800");'>BrightEdge</a>), 92.96% of global traffic comes from Google (<a id='reference-link' onclick='window.open("https://sparktoro.com/blog/2018-search-market-share-myths-vs-realities-of-google-bing-amazon-facebook-duckduckgo-more/", "_blank", "width=800, height=800");'>Sparktoro</a>), and only 0.78% of Google searchers click on results from the 2nd page (<a id='reference-link' onclick='window.open("https://backlinko.com/google-ctr-stats", "_blank", "width=800, height=800");'>Backlinko</a>)? If you want customers who are searching for your product or service to find you it is imperative that you use search engine optimization to bring your company up in the search results and avoid becoming a needle in a haystack.`,
 };
 
 export default paragraphs;
