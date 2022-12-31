0d10d238092611ef779fd7695816f28bec49c604
Chriscastanuela <chriscastanuela@ymail.com>
2022-12-30 23:53:27 -0700

    remove menu cover

--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
 import domElements from '/src/javascript/dom-elements.js';
 import functions from '/src/javascript/functions.js';
 
-onload = () => functions.theHiddenThing(domElements.menuCover);
-
-domElements.xButton.onclick = () => functions.openAndClose();
-domElements.hero.onclick = () => functions.openAndClose();
-domElements.services.onclick = () => functions.openAndClose();
-domElements.pricing.onclick = () => functions.openAndClose();
-domElements.contact.onclick = () => functions.openAndClose();
-// growth.onclick = () => openAndClose
-// whyEnvysion.onclick = () => openAndClose');
-
 domElements.scroll.forEach((el) => functions.observer.observe(el));
-
-// let count = 0;
-
-// domElements.quoraCreativeLink
-//   ? (domElements.quoraCreativeLink.onclick = () => {
-//       window.open(
-//         'https://quoracreative.com/article/mobile-marketing-statistics',
-//         '_blank',
-//         'width=800, height=800'
-//       );
-//     })
-//   : null;
-
-// domElements.nextButton.onclick = () => functions.carouselScrollNext();
--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -1,14 +1,7 @@
-import domElements from '/src/javascript/dom-elements.js';
-
 const theHiddenThing = (element) =>
   element.style.visibility == 'hidden'
     ? (element.style.visibility = 'visible')
     : (element.style.visibility = 'hidden');
-
-const openAndClose = () => {
-  domElements.xButton.classList.toggle('nav__button-x-open');
-  theHiddenThing(domElements.menuCover);
-};
 
 const observer = new IntersectionObserver((entries) =>
   entries.forEach((i) => i.isIntersecting && i.target.classList.add('show'))
@@ -21,17 +14,3 @@ const functions = {
 };
 
 export default functions;
-
-// i.isIntersecting
-//   ? i.target.classList.add('show')
-//   : i.target.classList.remove('show');
-
-// const carouselScrollNext = () => {
-//   if (count === 4) {
-//     count = 0;
-//     currentCarousel.innerHTML = carouselDivs[count];
-//   } else {
-//     count += 1;
-//     currentCarousel.innerHTML = carouselDivs[count];
-//   }
-// };
